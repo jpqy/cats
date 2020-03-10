065525219708cb656884976d91fbc42e07b6ebda
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-10 14:11:39 -0400

    implemented sync and async

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+    // We expect it to be a function, but will it be?
+    console.log(breedDetails);
+  });
+});
+
+// export the function
+module.exports = breedDetails;

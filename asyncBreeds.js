0375a717034b0047961fef8415b749d207de49ec
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-10 15:03:59 -0400

    implemented async test

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -4,18 +4,24 @@ const fs = require('fs');
 const breedDetailsFromFile = function(breed, cb) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log(`In readFile's Callback: it has the data.`);
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) cb(data);
+    if (error) {
+      cb(undefined);
+    } else {
+      console.log(`In readFile's Callback: it has the data.`);
+      // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+      cb(data);
+    };
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
 const printBreed = (data) => {
-  console.log('Return Value: ', data);  
-}
+  console.log('Return Value: ', data);
+};
+
+module.exports = breedDetailsFromFile;
 // we try to get the return value
-breedDetailsFromFile('Bombay', printBreed);
+//breedDetailsFromFile('Bombay', printBreed);
 
 
